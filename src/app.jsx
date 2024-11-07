ca19ec87156a2a183e7618e6055a3b8bcfaccd47
Pumpkin <mohammadmehdibeheshti@yahoo.com>
2024-11-07 23:31:17 +0330

    Update app.jsx

--- src/app.jsx
+++ src/app.jsx
@@ -1,10 +1,8 @@
 import GoodbyeWorld from "./components/GoodbyeWorld"
-import UserAvatar from "./components/UserAvatar"
 
 export default function App() {
   return (
     <>
-      {/* <UserAvatar size={48} showUsername={true} showId={true} username={"Admin"} id={"@admin"}/> */}
       <GoodbyeWorld />
     </>
   )
